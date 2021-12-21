086c17f6462d9cd6269257a68d62108a0f3a728e
Sushil-kafle <sushil.kafle@lec.edu.np>
2021-12-21 16:24:30 +0545

    added another function

--- bisection.c
+++ bisection.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#define error 0.0005
+#define error 0.000005
 
 float func(float x)
 {
-    float result;
-    result = pow(x, 4) - 32;
-    return result;
+    return pow(x, 4) - 32;
 }
+
+// float func(float x)
+// {
+
+//     return (2 * log(x) + 3);
+// }
+
+// float func(float x)
+// {
+//     double result = x;
+//     return (exp(result) + 3 * x - 4);
+// }
+
 float bisect(float a, float b, float c)
 {
     static int iteration = 0;
@@ -38,11 +49,11 @@ float bisect(float a, float b, float c)
 
 int main()
 {
-    int i, iteration = 0;
+    int i, iteration = 0, max_iteration = 100;
     float a = 0, b = 1, c, root;
     while (1)
     {
-        if (iteration == 100)
+        if (iteration == max_iteration)
         {
             printf("Cannot find value of a and b");
             exit(0);
